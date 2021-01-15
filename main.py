45cb2cb09b08eab56ce05c4241f12a93e04e3667
Elxan Mammedov <elxanmammed@mail.ru>
2021-01-15 14:08:46 +0400

    ilk commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import turtle
+
+pencere = turtle.Screen()
+pencere.title('yilan oyunu')
+pencere.bgcolor('lightgreen')
+pencere.setup(width=600,height=600)
+pencere.tracer(0)
+
+
+while True:
+    pencere.update()
